0ed2eb30e13b7fb96f9544e272781a2a203ea53e
Anton <kamarhin_anton@ukr.net>
2022-02-22 10:19:15 +0200

    feat: fix lottie top-5

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,14 +1,25 @@
+window.addEventListener('DOMContentLoaded', function () {
+    lottieAnimation()
+})
+
 const lottieAnimation = () => {
-    let animation = bodymovin.loadAnimation({
-        container: document.getElementById('lottie-1'),
-        path: 'plugins/json/lottie.json',
+    // let animation = bodymovin.loadAnimation({
+    //     container: document.getElementById('lottie-1'),
+    //     path: 'plugins/json/lottie.json',
+    //     renderer: 'canvas',
+    //     loop: false,
+    //     autoplay: true,
+    // })
+    let element = document.getElementById('lottie-1');
+    lottie.loadAnimation({
+        container: element,
         renderer: 'canvas',
         loop: false,
         autoplay: true,
-    })
+        path: 'plugins/json/lottie.json',
+    });
 }
 
-lottieAnimation()
 
 
 
