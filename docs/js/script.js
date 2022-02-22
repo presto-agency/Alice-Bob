8edf91b70152d2df2bde0154ee303f74f6a16f0c
Anton <kamarhin_anton@ukr.net>
2022-02-22 09:43:09 +0200

    feat: fix lottie top

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', function () {
 })
 
 const lottieAnimation = () => {
-    let animation = bodymovin.loadAnimation({
+    let topanimation = bodymovin.loadAnimation({
         container: document.getElementById('lottie-1'),
         path: 'plugins/json/lottie.json',
         renderer: 'canvas',
