9c434962a49f6801d1a1b5215282a85467622e75
Anton <kamarhin_anton@ukr.net>
2022-02-23 13:51:28 +0200

    feat: add animation to block

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,16 +1,25 @@
 window.addEventListener('DOMContentLoaded', function () {
     lottieAnimation()
+    removePreloader()
+    lottieInter()
+    addclass()
 })
 
 const lottieAnimation = () => {
-    let block_1_anim = bodymovin.loadAnimation({
-        container: document.getElementById('lottie-1'),
-        path: 'plugins/json/block-1-anim.json',
-        renderer: 'canvas',
-        loop: false,
-        autoplay: true,
-    })
+    function lottieTop() {
+        let block_1_anim = bodymovin.loadAnimation({
+            container: document.getElementById('lottie-1'),
+            path: 'plugins/json/block-1-anim.json',
+            renderer: 'canvas',
+            loop: false,
+            autoplay: true,
+        })
+    }
+    setTimeout(lottieTop, 4500)
 
+}
+
+const lottieInter = () => {
     LottieInteractivity.create({
         player: '#lottie-2',
         mode: "scroll",
@@ -23,6 +32,29 @@ const lottieAnimation = () => {
         ]
     });
 }
+
+const removePreloader = () => {
+    let preloader = document.querySelector('.preloader');
+    function removePreloader() {
+        preloader.remove()
+    }
+    setTimeout(removePreloader, 4500)
+}
+
+const addclass = () => {
+    let header = document.querySelector('.header');
+    let block = document.querySelector('.block-1');
+    addActive(header)
+    addActive(block)
+}
+
+function addActive(obj) {
+    const addClass = () => {
+        obj.classList.add('active')
+    }
+    setTimeout(addClass, 4500)
+}
+
 
 
 
